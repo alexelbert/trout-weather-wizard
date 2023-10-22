47360dda29e5827b0586c4b80ed3a617c8b16853
Alexander Elbert <alexander91elbert@gmail.com>
2023-10-22 17:48:13 +0200

    Added Func to access forecasted days weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,61 @@
 let cityInput = document.getElementById('city-input');
 let searchButton = document.querySelector('.search-btn');
-let currentWeatherDiv = document.querySelector('.weatherdetails');
+let currentWeatherDiv = document.querySelector('.currentweather');
+let forecastDiv = document.querySelector('.days');
 
+
+/**
+ * Returns the API values for current weather
+ */
 function weatherCard(weatherData, name) {
     let currentWeather = `<div class="weatherdetails">
-                    <h2>Weather in ${name} right now</h2>
-                    <h4>Cloud cover: ${weatherData.current.cloudcover} %</h4>
-                    <h4>Temperature: ${weatherData.current.temperature_2m} °C</h4>
-                    <h4>Wind: ${weatherData.current.windspeed_10m} km/h</h4>
-                    <h4>Wind direction: ${weatherData.current.winddirection_10m} °</h4>
-                    <h4>Sea pressure: ${weatherData.current.pressure_msl} hPa</h4>
-                    <h4>Precipitation: ${weatherData.current.precipitation} mm</h4>
-                </div>`;
+                                <h2>Weather in ${name} right now</h2>
+                                <h4>Cloud cover: ${weatherData.current.cloudcover} %</h4>
+                                <h4>Temperature: ${weatherData.current.temperature_2m} °C</h4>
+                                <h4>Wind: ${weatherData.current.windspeed_10m} km/h</h4>
+                                <h4>Wind direction: ${weatherData.current.winddirection_10m} °</h4>
+                                <h4>Sea pressure: ${weatherData.current.pressure_msl} hPa</h4>
+                                <h4>Precipitation: ${weatherData.current.precipitation} mm</h4>
+                            </div>
+                            <div class="icon">
+                                <img src="assets/images/390494_cloud_rain_shine_sun_weather_icon.png" alt="Weather icon">
+                            </div>`;
     return currentWeather;
 }
 
+/**
+ * Loops through the API daily response 
+ * and assigns each weather variable to the corresponding day
+ * @param {*} weatherData 
+ * @returns list of past and future weather
+ */
 
+function forecast(weatherData) {
+    let forecastDays = '';
+    let dayTitles = ['Two days ago', 'Yesterday', 'Today', 'Tomorrow']
+    for (let i = 0; i < 4; i++) {
+        let oneDay = `<li class="day-info">
+                            <h2>${dayTitles[i]}</h2>
+                            <img src="assets/images/390494_cloud_rain_shine_sun_weather_icon.png" alt="Weather icon">
+                            <h4>Temperature ${weatherData.daily.temperature_2m_min[i]}°C - ${weatherData.daily.temperature_2m_max[i]}°C</h4>
+                            <h4>Wind speed: ${weatherData.daily.windspeed_10m_max[i]} km/h</h4>
+                            <h4>Wind direction: ${weatherData.daily.winddirection_10m_dominant[i]}°</h4>
+                            <h4>Total precipitation: ${weatherData.daily.precipitation_sum[i]} mm</h4>
+                        </li>`
+        forecastDays = forecastDays.concat(oneDay)
+    }
+    
+    return forecastDays;
+}
+
+// TODO put this into weatherCard
 function updateWeatherCard(weatherData, name) {
     currentWeatherDiv.innerHTML = '';
     currentWeatherDiv.innerHTML = weatherCard(weatherData, name);
+
+    forecastDiv.innerHTML = '';
+    forecastDiv.innerHTML = forecast(weatherData);
+    
 }
 
 /**
@@ -38,6 +75,7 @@ function getWeather(name, latitude, longitude, timezone) {
         .then((weatherData) => {
             console.log(weatherData);
             updateWeatherCard(weatherData, name);
+            forecast(weatherData);
         })
         .catch(() => {
             alert("Can't find weather information");
