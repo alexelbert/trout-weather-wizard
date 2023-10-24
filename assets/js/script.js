1325333e6bf4f676c60855515bf7e882daae3434
Alexander Elbert <alexander91elbert@gmail.com>
2023-10-24 22:24:09 +0200

    Update let to const, added func to get user coords

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,21 @@
-let cityInput = document.getElementById('city-input');
-let searchButton = document.querySelector('.search-btn');
-let currentWeatherDiv = document.querySelector('.currentweather');
-let forecastDiv = document.querySelector('.days');
+const cityInput = document.getElementById('city-input');
+const searchButton = document.querySelector('.search-btn');
+const locationButton = document.querySelector('.location-btn')
+const currentWeatherDiv = document.querySelector('.currentweather');
+const forecastDiv = document.querySelector('.days');
+
 
 
 /**
  * Returns the API values for current weather
  */
 function weatherCard(weatherData, name) {
-    let currentWeather = `<div class="weatherdetails">
+    const currentWeather = `<div class="weatherdetails">
                                 <h2>Weather in ${name} right now</h2>
-                                <h4>Cloud cover: ${weatherData.current.cloudcover} %</h4>
-                                <h4>Temperature: ${weatherData.current.temperature_2m} °C</h4>
+                                <h4>Cloud cover: ${weatherData.current.cloudcover}%</h4>
+                                <h4>Temperature: ${weatherData.current.temperature_2m}°C</h4>
                                 <h4>Wind: ${weatherData.current.windspeed_10m} km/h</h4>
-                                <h4>Wind direction: ${weatherData.current.winddirection_10m} °</h4>
+                                <h4>Wind direction: ${weatherData.current.winddirection_10m}°</h4>
                                 <h4>Sea pressure: ${weatherData.current.pressure_msl} hPa</h4>
                                 <h4>Precipitation: ${weatherData.current.precipitation} mm</h4>
                             </div>
@@ -32,9 +34,9 @@ function weatherCard(weatherData, name) {
 
 function forecast(weatherData) {
     let forecastDays = '';
-    let dayTitles = ['Two days ago', 'Yesterday', 'Today', 'Tomorrow']
+    const dayTitles = ['Two days ago', 'Yesterday', 'Today', 'Tomorrow']
     for (let i = 0; i < 4; i++) {
-        let oneDay = `<li class="day-info">
+        const oneDay = `<li class="day-info">
                             <h2>${dayTitles[i]}</h2>
                             <img src="assets/images/390494_cloud_rain_shine_sun_weather_icon.png" alt="Weather icon">
                             <h4>Temperature ${weatherData.daily.temperature_2m_min[i]}°C - ${weatherData.daily.temperature_2m_max[i]}°C</h4>
@@ -73,7 +75,6 @@ function getWeather(name, latitude, longitude, timezone) {
             return response.json();
         })
         .then((weatherData) => {
-            console.log(weatherData);
             updateWeatherCard(weatherData, name);
             forecast(weatherData);
         })
@@ -88,7 +89,7 @@ function getWeather(name, latitude, longitude, timezone) {
  * for the weather API
  */
 function getCityCoordinates() {
-    let cityName = cityInput.value.trim(); // Get user input and trim whitespace
+    const cityName = cityInput.value.trim(); // Get user input and trim whitespace
     if (!cityName) return; // Returns nothing if cityName is empty
     const geocodingUrl = `https://geocoding-api.open-meteo.com/v1/search?name=${cityName}&count=1&language=en&format=json`;
 
@@ -101,12 +102,42 @@ function getCityCoordinates() {
         })
         .then((geocodingData) => {
             if (!geocodingData.results) return alert(`No coordinates found for ${cityName}`);
-            let { name, latitude, longitude, timezone } = geocodingData.results[0];
+            const { name, latitude, longitude, timezone } = geocodingData.results[0];
             getWeather(name, latitude, longitude, timezone);
+            cityInput.value = '';
         })
         .catch(() => {
             alert("Can't fetch the coordinates");
         });
 }
 
-searchButton.addEventListener('click', getCityCoordinates);
+/**
+ * Get user current location and use it as input 
+ * to fetch weather data 
+ * Note: No reverse geocoding in the API, used placeholder text instead
+ * of city name and auto timezone
+ */
+
+function getUserCoordinates() {
+    navigator.geolocation.getCurrentPosition(function(userCoordinates) {
+        const userLatitude = userCoordinates.coords.latitude;
+        const userLongitude = userCoordinates.coords.longitude;
+        getWeather('your location', userLatitude, userLongitude, 'auto')
+    });
+}
+
+locationButton.addEventListener('click', function() {
+    getUserCoordinates();
+});
+
+searchButton.addEventListener('click', function() {
+    getCityCoordinates();
+});
+
+cityInput.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        getCityCoordinates();
+        cityInput.value = '';
+    }
+})
+
